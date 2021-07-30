e2d7e0fc2404d2eb5d4add246e9ae6a34c4bff7d
rjmata95 <12-11030@usb.ve>
2021-07-30 12:03:46 -0400

    solving environment issues

--- Server/index.js
+++ Server/index.js
@@ -1,29 +1,31 @@
-const express = require('express')
-const cors = require('cors')
-const routes = require('./network/routes')
-const mongoose = require('mongoose')
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const routes = require("./network/routes");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-var app = express()
-
-const PORT = process.env.PORT || 5000
-const MONGO_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+var app = express();
+const { DB_USER, DB_PASSWORD, DB_NAME, DB_HOST, DB_PORT, SERVER_PORT } =
+  process.env;
+const PORT = SERVER_PORT || 5000;
+const MONGO_URI = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?retryWrites=true&w=majority`;
 
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: false }));
 // app.use("/", express.static("app"));
 app.use(cors());
-routes(app)
+routes(app);
 
-mongoose.connect(MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
-  .then(()=>{
-      app.listen(PORT,()=>{
-          console.log(`Listening on port: ${PORT}`)
-      })
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Listening on port: ${PORT}`);
+    });
   })
-  .catch((error)=>{
-    console.error(`Error connecting: ${error}`)
-  })
-  
-mongoose.set('useFindAndModify', false)
+  .catch((error) => {
+    console.error(`Error connecting: ${error}`);
+  });
+
+mongoose.set("useFindAndModify", false);
